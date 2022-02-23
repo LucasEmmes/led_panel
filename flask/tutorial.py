da8d85e0d7b9121a6c73625c0f1f01f109dc10ef
LucasEmmes <lucas.f.emmes@protonmail.com>
2022-02-23 10:50:35 +0100

    Cazn now show triasngles at all

--- flask/tutorial.py
+++ flask/tutorial.py
@@ -3,15 +3,15 @@ import led_control
 
 app = Flask(__name__)
 
+size = 200
 # x,y,usd,size,color
-triangle_data="0,0,1,100,#FFFF00;50,0,0,100,#FF00FF"
+triangle_data=f"0,0,1,{size},#FF0000;100,0,0,{size},#0000FF"
 
-# Pages
-@app.route("/")
+
+@app.route("/home/")
 def home():
-    # return "Hello homepage<h1>HELLO</h1>"
-    # return render_template("index.html", content=name, r=2, l=["tim","jo","bill"])
-    return render_template("index.html")
+    return render_template("led_controller.html")
+
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
@@ -21,18 +21,24 @@ def login():
     else:
         return render_template("login.html")
 
-@app.route("/test")
-def test():
-    return render_template("canvas.html", td=triangle_data)
 
-@app.route("/<usr>")
-def user(usr):
-    return f"<h1>{usr}</h1>"
+@app.route("/test", methods=["POST", "GET"])
+def test():
+    if request.method == "POST":
+        td = request.form["triangle_1"]
+        r,g,b = post_string_to_rgb(td)
+        # led_control.set_led(r,g,b)
+        return render_template("canvas.html", td=td)
+    return render_template("canvas.html", td=triangle_data)
 
 @app.route("/color/<c>")
 def color(c):
     led_control.set_led(int(c))
     return ""
+
+def post_string_to_rgb(ps:str):
+    r,g,b = [int(i, 16) for i in [ps.split(",")[-1][1:][x:x+2] for x in range(0, 6, 2)]]
+    return r,g,b
 
 if __name__=="__main__":
     app.run(debug=True)
