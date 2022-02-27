0c08b2ea82ae1546a497996f34ba328d2db87563
LucasEmmes <lucas.f.emmes@protonmail.com>
2022-02-27 12:30:03 +0100

    Can now click triangles to select them

--- led_server/insert_triangles.py
+++ led_server/insert_triangles.py
@@ -10,10 +10,18 @@ def main():
         TriangleData(0, 0, "static", (255,0,0), 1, 0, 0, 0, [_ for _ in range(0,6)]),
         TriangleData(1, 0, "static", (255,0,255), 1, 0, 0, 0, [_ for _ in range(6,12)]),
         TriangleData(1, 1, "static", (255,0,0), 1, 0, 0, 0, [_ for _ in range(12,18)]),
-        TriangleData(2, 1, "static", (255,0,255), 1, 0, 0, 0, [_ for _ in range(18,24)]),
-        TriangleData(3, 1, "static", (255,0,0), 1, 0, 0, 0, [_ for _ in range(24,30)]),
-        TriangleData(4, 1, "static", (255,0,255), 1, 0, 0, 0, [_ for _ in range(30,36)]),
-        TriangleData(4, 2, "static", (255,0,0), 1, 0, 0, 0, [_ for _ in range(36,42)]),
+        TriangleData(0, 1, "static", (255,0,255), 1, 0, 0, 0, [_ for _ in range(12,18)]),
+        TriangleData(1, 2, "static", (0,255,0), 1, 0, 0, 0, [_ for _ in range(12,18)]),
+        TriangleData(0, 2, "static", (255,255,0), 1, 0, 0, 0, [_ for _ in range(12,18)]),
+        # TriangleData(-1, 0, "static", (255,0,255), 1, 0, 0, 0, [_ for _ in range(6,12)]),
+        TriangleData(2, 0, "static", (255,0,0), 1, 0, 0, 0, [_ for _ in range(0,6)]),
+        TriangleData(3, 0, "static", (255,0,255), 1, 0, 0, 0, [_ for _ in range(6,12)]),
+        TriangleData(3, 1, "static", (255,0,0), 1, 0, 0, 0, [_ for _ in range(12,18)]),
+        TriangleData(2, 1, "static", (255,0,255), 1, 0, 0, 0, [_ for _ in range(12,18)]),
+        # TriangleData(2, 1, "static", (255,0,255), 1, 0, 0, 0, [_ for _ in range(18,24)]),
+        # TriangleData(3, 1, "static", (255,0,0), 1, 0, 0, 0, [_ for _ in range(24,30)]),
+        # TriangleData(4, 1, "static", (255,0,255), 1, 0, 0, 0, [_ for _ in range(30,36)]),
+        # TriangleData(4, 2, "static", (255,0,0), 1, 0, 0, 0, [_ for _ in range(36,42)]),
     ]
     
     with open('triangle_data.pickle', 'wb') as f:
